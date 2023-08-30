c3155d48a672bd772b6c7ded53bbcf78ae2d15cb
stropee <stropee@campus-eni.fr>
2023-08-30 13:54:36 +0200

    exemple insertion, update, delete serie

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,8 +28,41 @@ class SerieController extends AbstractController
     }
 
     #[Route('/new', name: 'new')]
-    public function new(): Response
+    public function new(EntityManagerInterface $entityManager): Response
     {
+
+        $serie = new Serie();
+        $serie
+            ->setBackdrop("backdrop")
+            ->setDateCreated(new \DateTime())
+            ->setGenres("SF")
+            ->setName("X-Files")
+            ->setFirstAirDate(new \DateTime("-10 year"))
+            ->setPopularity(500)
+            ->setPoster('poster.png')
+            ->setStatus('ending')
+            ->setTmdbId(1234)
+            ->setVote(8);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        //update du nom de la série
+        $serie->setName("Code Quantum");
+        dump($serie);
+        //ça lance un update et non une nouvelle insertion
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
         //TODO renvoyer un formulaire d'ajout de série
         return $this->render('serie/new.html.twig');
     }
